9cc90283aaba95c923d5550b8c2801085898a4cb
Mmarin95 <9479635+Mmarin95@users.noreply.github.com>
2024-04-05 09:01:40 +0200

    Add font installation script

---
+++ install_scripts/fonts.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Debian recommended fonts seems to make everything look better after its installation and reboot
+
+# Install fonts
+sudo apt install fonts-recommended -y
+
+# Install liberation that mimics the look of some Microsoft fonts
+sudo apt install fonts-liberation fonts-liberation2 -y
