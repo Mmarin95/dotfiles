aa8cd37ec56ee12d0b6e90f9dfb1d77cb6c77eca
Mmarin95 <9479635+Mmarin95@users.noreply.github.com>
2024-07-19 15:59:26 +0200

    init lua but better

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,25 +1,10 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>') 
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>') 
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>') 
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
--- Set highlight on search, but clear on pressing <Esc> in normal mode 
-vim.opt.hlsearch = true 
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
--- Diagnostics
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' }) 
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' }) 
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' }) 
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
 vim.opt.undofile = true
--- vim.opt.undodir = $HOME/.config/nvim/undo
--- set undolevels=10000
--- set undoreload=10000
+vim.opt.undodir = vim.fn.expand('$HOME/.config/nvim/undo')
+vim.opt.undolevels=10000
+vim.opt.undoreload=10000
 
 vim.opt.mouse = 'a'
 -- vim.opt.showmode = false
@@ -39,6 +24,44 @@ vim.opt.number = true
 vim.opt.breakindent = true
 vim.opt.inccommand = 'split'
 
-vim.opt.scrolloff = 10
 -- TODO: For tabstop, shiftwidth 'tpope/vim-sleuth'
+vim.opt.tabstop = 4 
+vim.opt.expandtab = true 
+vim.opt.softtabstop = 4 
+vim.opt.shiftwidth = 4
+
+vim.opt.scrolloff = 10
+
+-- [[ Basic keymaps ]]
+
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>') 
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>') 
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>') 
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Set highlight on search, but clear on pressing <Esc> in normal mode 
+vim.opt.hlsearch = true 
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostics
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' }) 
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' }) 
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' }) 
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Move focus windows
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' }) 
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' }) 
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' }) 
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' }) 
+
+-- [[ Autocommands ]]
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking text',   
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),   
+  callback = function()     
+    vim.highlight.on_yank()   
+  end, 
+})
 
