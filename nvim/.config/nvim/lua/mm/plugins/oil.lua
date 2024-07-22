a3bfecd06499212f04ef03c1f832d05240726467
Mmarin95 <9479635+Mmarin95@users.noreply.github.com>
2024-07-22 08:28:08 +0200

    add oil

---
+++ nvim/.config/nvim/lua/mm/plugins/oil.lua
@@ -0,0 +1,10 @@
+-- TODO: Add options like columns and so on
+return {
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		-- Optional dependencies
+		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+	},
+}
