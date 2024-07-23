d484c100e5664990bf91e79ae84c9f3f6fc4d3f1
Mmarin95 <9479635+Mmarin95@users.noreply.github.com>
2024-07-23 21:54:39 +0200

    add - key for oil

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,68 +1,70 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 vim.opt.undofile = true
-vim.opt.undodir = vim.fn.expand('$HOME/.config/nvim/undo')
-vim.opt.undolevels=10000
-vim.opt.undoreload=10000
+vim.opt.undodir = vim.fn.expand("$HOME/.config/nvim/undo")
+vim.opt.undolevels = 10000
+vim.opt.undoreload = 10000
 
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 -- vim.opt.showmode = false
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 
-vim.opt.ignorecase = true 
+vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
-vim.opt.updatetime = 250 
--- Decrease mapped sequence wait time 
--- Displays which-key popup sooner 
+vim.opt.updatetime = 250
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
 vim.opt.timeoutlen = 300
 
 vim.opt.number = true
 -- vim.opt.relativenumber = true
 vim.opt.breakindent = true
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 
 -- TODO: For tabstop, shiftwidth 'tpope/vim-sleuth'
-vim.opt.tabstop = 4 
-vim.opt.expandtab = true 
-vim.opt.softtabstop = 4 
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 
 vim.opt.scrolloff = 10
 
 -- [[ Basic keymaps ]]
 
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>') 
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>') 
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>') 
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 
--- Set highlight on search, but clear on pressing <Esc> in normal mode 
-vim.opt.hlsearch = true 
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostics
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' }) 
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' }) 
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' }) 
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- Move focus windows
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' }) 
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' }) 
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' }) 
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' }) 
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- [[ Autocommands ]]
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking text',   
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),   
-  callback = function()     
-    vim.highlight.on_yank()   
-  end, 
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 require("mm.config.lazy")
