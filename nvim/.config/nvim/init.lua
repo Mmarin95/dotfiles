0cf48f3956967a8e39a382e17f1a0a940b457d2c
Mmarin95 <9479635+Mmarin95@users.noreply.github.com>
2024-07-25 15:02:28 +0200

    Move keymaps after load plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -32,6 +32,19 @@ vim.opt.shiftwidth = 4
 
 vim.opt.scrolloff = 10
 
+-- [[ Autocommands ]]
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+require("mm.config.lazy")
+-- load keymaps after plugins
+
 -- [[ Basic keymaps ]]
 
 vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
@@ -39,11 +52,11 @@ vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "-", "<Cmd>Oil<CR>", { desc = "Open parent directory" })
 
--- Navigate buffers with [b and ]b
-vim.keymap.set("n", "[b", ":bprev<CR>", { noremap = true, desc = "Go to previous buffer" })
-vim.keymap.set("n", "]b", ":bnext<CR>", { noremap = true, desc = "Go to next buffer" })
+-- TODO: Why is this not working?
+vim.keymap.set("n", "[b", "<Cmd>bprev<CR>")
+vim.keymap.set("n", "]b", "<Cmd>bnext<CR>")
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
@@ -60,15 +73,3 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
--- [[ Autocommands ]]
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking text",
-	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
-
-require("mm.config.lazy")
