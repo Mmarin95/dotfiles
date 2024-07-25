cca6b53501afec1f0f0ca1b97ca16508c204ed1c
Mmarin95 <9479635+Mmarin95@users.noreply.github.com>
2024-07-25 15:48:11 +0200

    changed alias for pacman fzf function

--- zsh/.config/zsh/scripts.sh
+++ zsh/.config/zsh/scripts.sh
@@ -4,11 +4,11 @@ compress() {
     tar cvzf $1.tar.gz $1
 }
 
-install() {
+in() {
     pacman -Slq | fzf --multi --preview 'pacman -Si {1}' | xargs -ro sudo pacman -S
 }
 
-remove() {
+re() {
     pacman -Qq | fzf --multi --preview 'pacman -Qi {1}' | xargs -ro sudo pacman -Rns
 }
 
