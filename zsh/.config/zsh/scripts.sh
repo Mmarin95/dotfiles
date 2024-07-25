5b16d5c3a3db1584221f59de1785299466cdeaf3
Mmarin95 <9479635+Mmarin95@users.noreply.github.com>
2024-07-25 15:04:14 +0200

    add note and todo functions

--- zsh/.config/zsh/scripts.sh
+++ zsh/.config/zsh/scripts.sh
@@ -11,3 +11,46 @@ install() {
 remove() {
     pacman -Qq | fzf --multi --preview 'pacman -Qi {1}' | xargs -ro sudo pacman -Rns
 }
+
+# From arch wiki
+note () {
+    # if file doesn't exist, create it
+    if [[ ! -f $HOME/.notes ]]; then
+        touch "$HOME/.notes"
+    fi
+
+    if ! (($#)); then
+        # no arguments, print file
+        cat "$HOME/.notes"
+    elif [[ "$1" == "-c" ]]; then
+        # clear file
+        printf "%s" > "$HOME/.notes"
+    else
+        # add all arguments to file
+        printf "%s\n" "$*" >> "$HOME/.notes"
+    fi
+}
+
+# From arch wiki. Adapted to work with zsh
+todo() {
+    if [[ ! -f $HOME/.todo ]]; then
+        touch "$HOME/.todo"
+    fi
+
+    if (( $# == 0 )); then
+        cat "$HOME/.todo"
+    elif [[ "$1" == "-l" ]]; then
+        nl -b a "$HOME/.todo"
+    elif [[ "$1" == "-c" ]]; then
+       : > "$HOME/.todo"
+    elif [[ "$1" == "-r" ]]; then
+        nl -b a "$HOME/.todo"
+        eval "printf '%.0s-' {1..${COLUMNS:-$(tput cols)}}"; echo
+        print -n "Type a number to remove: "
+        read number
+        sed -i "${number}d" "$HOME/.todo"
+    else
+        printf "%s\n" "$*" >> "$HOME/.todo"
+    fi
+}
+
