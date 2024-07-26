4327663ec84d2138cefd9a72e291bc5414f9cb1f
Mmarin95 <9479635+Mmarin95@users.noreply.github.com>
2024-07-26 08:27:06 +0200

    add nvim-ts-context-commentstring

--- nvim/.config/nvim/lua/mm/plugins/comment-nvim.lua
+++ nvim/.config/nvim/lua/mm/plugins/comment-nvim.lua
@@ -1,3 +1,23 @@
+-- [[
+-- Integration with 'JoosepAlviste/nvim-ts-context-commentstring'
+--
+-- vim.cmd is there to try. gcc should comment with "
+-- -> uncomment with gcc, comment echo line with gcc
+-- and lua code should be commented with --
+-- ]]
+
+-- vim.cmd([[
+-- echo 'Hello World!'
+-- ]])
+
 return {
-    { 'numToStr/Comment.nvim', opts = {} },
+	-- { "numToStr/Comment.nvim", opts = {} },
+	{
+		"numToStr/Comment.nvim",
+		opts = {
+			pre_hook = function()
+				return vim.bo.commentstring
+			end,
+		},
+	},
 }
