f7fd78328582cad445067bad78eec88726ef58fb
Mmarin95 <9479635+Mmarin95@users.noreply.github.com>
2024-07-26 11:15:25 +0200

    remove vim sleuth overlaping options, disable telescope and lsp-binds for telescope, use native vim.lsp actions

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -24,11 +24,10 @@ vim.opt.number = true
 vim.opt.breakindent = true
 vim.opt.inccommand = "split"
 
--- TODO: For tabstop, shiftwidth 'tpope/vim-sleuth'
-vim.opt.tabstop = 4
-vim.opt.expandtab = true
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+-- vim.opt.tabstop = 4
+-- vim.opt.expandtab = true
+-- vim.opt.softtabstop = 4
+-- vim.opt.shiftwidth = 4
 
 vim.opt.scrolloff = 10
 
