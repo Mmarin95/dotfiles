a4fd86948620ab950a00033f2f93bd02c4c390dc
Mmarin95 <9479635+Mmarin95@users.noreply.github.com>
2024-07-26 13:59:03 +0200

    add better cmp sources

--- nvim/.config/nvim/lua/mm/plugins/lsp-zero.lua
+++ nvim/.config/nvim/lua/mm/plugins/lsp-zero.lua
@@ -6,6 +6,7 @@ return {
 			"neovim/nvim-lspconfig",
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/nvim-cmp",
+			'hrsh7th/cmp-buffer',
 			"L3MON4D3/LuaSnip",
 			{ "williamboman/mason.nvim" },
 			{ "williamboman/mason-lspconfig.nvim" },
@@ -17,8 +18,15 @@ return {
 			lsp_zero.on_attach(function(client, bufnr)
 				-- see :help lsp-zero-keybindings
 				-- to learn the available actions
-				lsp_zero.default_keymaps({ buffer = bufnr })
+				lsp_zero.default_keymaps({ buffer = bufnr, preserve_mappings = false })
 			end)
+
+			lsp_zero.set_sign_icons({
+				error = '✘',
+				warn = '▲',
+				hint = '⚑',
+				info = '»'
+			})
 
 			-- manually lua_ls if we had installed it manually
 			-- lspconfig.lua_ls.setup({})
@@ -38,8 +46,10 @@ return {
 			-- Enable completion with nvim-cmp
 			local cmp = require("cmp")
 			local cmp_action = require("lsp-zero").cmp_action()
+			local cmp_format = require('lsp-zero').cmp_format({ details = true })
 
 			cmp.setup({
+				formatting = cmp_format, -- Shows where does the completion come from
 				mapping = cmp.mapping.preset.insert({
 					-- `Enter` key to confirm completion
 					["<CR>"] = cmp.mapping.confirm({ select = false }),
@@ -55,6 +65,11 @@ return {
 					["<C-u>"] = cmp.mapping.scroll_docs(-4),
 					["<C-d>"] = cmp.mapping.scroll_docs(4),
 				}),
+				sources = {
+					{ name = "luasnip" },
+					{ name = "nvim_lsp" },
+					{ name = "buffer" },
+				},
 				snippet = {
 					expand = function(args)
 						require("luasnip").lsp_expand(args.body)
