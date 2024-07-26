13da4389aaa9f956733fb67609ec63643bbaec95
Mmarin95 <9479635+Mmarin95@users.noreply.github.com>
2024-07-26 15:22:04 +0200

    removed old code

--- nvim/.config/nvim/lua/mm/plugins/lsp-zero.lua
+++ nvim/.config/nvim/lua/mm/plugins/lsp-zero.lua
@@ -8,6 +8,7 @@ return {
 			"hrsh7th/nvim-cmp",
 			'hrsh7th/cmp-buffer',
 			"L3MON4D3/LuaSnip",
+			{ 'saadparwaiz1/cmp_luasnip' },
 			{ "williamboman/mason.nvim" },
 			{ "williamboman/mason-lspconfig.nvim" },
 		},
