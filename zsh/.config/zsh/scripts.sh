fc92d21855ee94b0f10008b4563b6689959a04c2
Mmarin95 <9479635+Mmarin95@users.noreply.github.com>
2024-08-08 13:09:57 +0200

    add fuzzy yay install and remove

--- zsh/.config/zsh/scripts.sh
+++ zsh/.config/zsh/scripts.sh
@@ -8,8 +8,16 @@ in() {
     pacman -Slq | fzf --multi --preview 'pacman -Si {1}' | xargs -ro sudo pacman -S
 }
 
+yin() {
+    yay -Slaq | fzf --multi --preview 'yay -Si {1}' | xargs -ro yay -S
+}
+
 re() {
     pacman -Qq | fzf --multi --preview 'pacman -Qi {1}' | xargs -ro sudo pacman -Rns
+}
+
+yre() {
+    pacman -Qqm | fzf --multi --preview 'yay -Qi {1}' | xargs -ro yay -Rns
 }
 
 # From arch wiki
